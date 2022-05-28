722341b04bd00b32dc3c646e1b2ed2fdb9c1f688
João Vítor Café <batistajv2012@gmail.com>
2022-05-28 19:00:34 -0300

    all-delete

--- TesteArvore.java
+++ TesteArvore.java
@@ -12,16 +12,49 @@ public class TesteArvore {
             t.insert(t.getRoot() , 8 );
             t.insert(t.getRoot() , 9 );
             t.insert(t.getRoot() , 3 );
+            t.insert(t.getRoot() , 10);
             t.insert(t.getRoot() , 2 );
-            t.insert(t.getRoot() , 10 );
             
-            System.out.println("\n Pre Ordem");
+            System.out.print("Pre Ordem = ");
             t.preOrder(t.getRoot());
-            
-            System.out.println("\n Em Ordem"); 
+            System.out.println("\n");
+
+            System.out.print("Em Ordem = "); 
             t.inOrder(t.getRoot());
+            System.out.println("\n");
             
-            System.out.println("\n Pos Ordem");
+            System.out.print("Pos Ordem = ");
             t.posOrder(t.getRoot());
-    }
+            System.out.println("\n");
+    
+            No target = t.search(t.getRoot() , 5);
+            if(target != null) {
+                System.out.println("Target = " + target.getValor());
+            } else {
+                System.out.println("Não encontrado");
+            }
+
+            No max = t.NoMax(t.getRoot());
+            System.out.println("Max = " + max.getValor());
+
+            No min = t.NoMin(t.getRoot());
+            System.out.println("Min = " + min.getValor());
+
+            int totalNos = t.totalNos(t.getRoot());
+            System.out.println("Total de nós = " + totalNos);
+
+            int totalFolhas = t.totalFolhas(t.getRoot());
+            System.out.println("Total de folhas = " + totalFolhas);
+
+            System.out.print("Folhas = ");
+            t.identificarFolhas(t.getRoot());
+            System.out.println("\n");
+        
+            int nivel = t.nivelNo(t.getRoot() , 10 , 0);
+            System.out.println("Nivel = " + nivel);
+        
+            int altura = t.alturaNo(t.getRoot() , 5);
+            System.out.println("Altura = " + altura);
+        
+        }
 }
--- Tree.java
+++ Tree.java
@@ -62,4 +62,137 @@ public class Tree {
 
         return atual;
     }
+
+    public No search(No atual , int valor) {
+        if(atual == null) {
+            return atual;
+        }
+
+        else if(atual.getValor() == valor){
+            return atual;
+        }
+   
+        else if (valor < atual.getValor()) {
+            return search(atual.getEsq() , valor);
+        } 
+        
+        else if (valor > atual.getValor()) {
+            return search(atual.getDir() , valor);
+        }    
+
+        return atual;
+    }
+
+    public No NoMax(No atual) {
+    // Caso a árvore não esteja em regra binária basta colocar pra ele percorrer à esquerda tbm.
+
+        if(atual != null) {
+            No maximo = atual;
+            No dir =  NoMax(atual.getDir());
+            
+            if(dir.getValor() > maximo.getValor())
+                maximo = dir;
+                return maximo;
+            }
+
+        return new No(Integer.MIN_VALUE);
+    }
+
+    public No NoMin(No atual) {
+        // Caso a árvore não esteja em regra binária basta colocar pra ele percorrer à direita tbm.
+        
+        if(atual != null) {
+            No minimo = atual;
+            No esq = NoMin(atual.getEsq());
+            
+            if(esq.getValor() < minimo.getValor())
+                minimo = esq;
+                return minimo;
+            }
+
+        return new No(Integer.MAX_VALUE);
+    }
+
+    public int totalNos(No atual) {
+        if(atual == null) {
+            return 0;
+        }
+
+        return 1 + totalNos(atual.getEsq()) + totalNos(atual.getDir());
+    }
+
+    public int totalFolhas(No atual) {
+        if(atual == null) {
+            return 0;
+        }
+        
+        if(atual.getEsq() == null && atual.getDir() == null) {
+            return 1;
+        } 
+
+        return totalFolhas(atual.getEsq()) + totalFolhas(atual.getDir());
+    }
+
+    public void identificarFolhas(No atual) {
+        
+        if(atual != null)
+        {
+            if(atual.getEsq() == null && atual.getDir() == null) {
+                System.out.print(atual.getValor() + " ");
+            } else {
+                identificarFolhas(atual.getEsq());
+                identificarFolhas(atual.getDir());
+            } 
+        }
+    }
+
+    public int nivelNo(No atual , int target , int nivel) {
+
+        if(atual == null){
+            return -1;
+        }
+
+        if(atual.getValor() == target){
+            return nivel;
+        }
+
+        int n = nivelNo(atual.getEsq() , target , nivel +1);
+
+        if(n != -1){
+            return n;
+        }
+
+        return nivelNo(atual.getDir() , target , nivel +1);
+    }
+
+    public int altura(No atual) {
+          
+        if(atual == null) {
+            return -1;
+        }
+
+        int alturaEsq = altura(atual.getEsq());
+        int alturaDir = altura(atual.getDir());
+
+        if(alturaEsq > alturaDir) {
+            return 1 + alturaEsq;
+        } else {
+            return 1 + alturaDir;
+        }
+         
+    }
+
+    public int alturaNo(No atual , int valor) {
+        No target = search(atual , valor);
+        if(target == null){
+            return -1;
+        } else {
+            return altura(target);
+        }
+    
+    }
 }
+
+    // identificar o nível de nó
+    // Altura de um nó    
+    // remover um nó
